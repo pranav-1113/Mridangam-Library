43790a61e75f9461562b702af29011b7c9ceb2c6
pranav-1113 <pranav.balakrishnan.21@gmail.com>
2026-07-09 17:39:51 -0500

    Update supabase_config.py

--- Mridangam_Library/supabase_config.py
+++ Mridangam_Library/supabase_config.py
@@ -1,8 +1,9 @@
+import streamlit as st
 from supabase import create_client
 
-SUPABASE_URL = st.secrets["https://goszsnddiyhzyhcnmrtl.supabase.co"]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
 
-SUPABASE_KEY = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdvc3pzbmRkaXloenloY25tcnRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0ODgyODgsImV4cCI6MjA5ODA2NDI4OH0.jB1vSYwf867nRVZExKrnuUFBsGPEAyPTPGWrhvQDj9M"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(
 SUPABASE_URL,
