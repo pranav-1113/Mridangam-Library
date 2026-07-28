c8a33b7aae934fbab76ce103ed8080601bc54d9d
pranav-1113 <pranav.balakrishnan.21@gmail.com>
2026-07-28 14:03:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,8 @@ elif page == "Notes":
 		selected_notes=st.selectbox("Choose a PDF",[pdf.name for pdf in pdfs])
 		pdfpath=notes_dir/selected_notes
 
+		file_url=(supabase.storage.from_("community-files").get_public_url(path))
+
 		with open(pdfpath,"rb") as f:
 			pdf1=f.read()
 			st.link_button(
